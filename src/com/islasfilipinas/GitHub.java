b42f8b2fe5408df6042d06719877601739604a77
DAM102V <DAM102V@DAM102>
2023-03-14 18:45:06 +0100

    Subir progrma GiHub

---
+++ src/com/islasfilipinas/GitHub.java
@@ -0,0 +1,13 @@
+package com.islasfilipinas;
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***Inicio de programa***");
+		System.out.println("Aparecerá en la pantalla:");
+		System.out.println("Hola Mundo");
+		System.out.println("***Fin de programa***");
+	}
+
+}
